8489e90de57465f997e06d85d33c18794e1744a5
Damien <damien.deboudt@sfr.fr>
2023-01-04 12:13:57 +0100

    ok

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let CpteARebours = function (Minutes, Secondes) {
-    this.Minutes = Minutes
-    this.Seconds = Secondes
-}
 
-let minutes = document.getElementById("timer-Minutes");
-let seconds = document.getElementById("timer-Seconds");
+let minutes = document.getElementById("Minutes");
+let seconds = document.getElementById("Seconds");
+let buttonSubmit = document.getElementById("LancerCRB");
+let divM = document.getElementById("M");
+let divS = document.getElementById("S");
 
-const boutonStart = document.getElementById('start');
-const boutonStop = document.getElementById('stop');
-const boutonReset = document.getElementById('reset');
 
-boutonStart.addEventListener("click", ()=> {
-  document.getElementById("compte-à-rebours").innerText = minutes.value + ":" + seconds.value
+buttonSubmit.addEventListener("click",()=>{
+    let interval = setInterval(function (){
+        divM.innerHTML = minutes.value
+        divS.innerHTML = seconds.value --
+
+        if (seconds.value == 0) {
+            parseInt(seconds.value += 59)
+            divM.innerHTML = parseInt(minutes.value--)
+        }
+        else if (minutes.value < 0 && seconds.value == 58) {
+            clearInterval(interval)
+            divM.innerHTML = ""
+            divS.innerHTML = ""
+            seconds.value = 0
+            minutes.value = 0
+        }
+
+    },100)
 })
-
-let timer = new CpteARebours(minutes.value, seconds.value)
